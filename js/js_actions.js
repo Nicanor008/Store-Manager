4045c1ec4c58f1820587f72eae69791a137fdc4d
Nicanor008 <nicanorkorir008@gmail.com>
2018-10-09 23:08:39 +0300

    [ft-#161039893]Have created a page for the store attendant to add products to cart and on click(Add To Cart button), a sale records is generated

---
+++ js/js_actions.js
@@ -0,0 +1,21 @@
+
+function AddToCart() {
+    let userclick = confirm("Do you want to Add to Cart?");
+    if (userclick == true) {
+       alert('One Product added to Cart');
+    }
+}
+
+
+// cart checkout
+function CartSellProducts() {
+    alert('Products Sold. Receipt Printing.')
+}
+
+// cancelling users cart
+function CancelCart() {
+    let clickconfirm = confirm("Are you sure you want to delete the Cart?");
+    if (clickconfirm == true) {
+        window.open('home.html', '_SELF');
+    }
+}
