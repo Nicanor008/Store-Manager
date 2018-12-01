03e86335182dcebdb4063354357cb082c086a678
Nicanor008 <nicanorkorir008@gmail.com>
2018-12-01 11:30:52 +0300

    [ft #161889191]post single sale

--- UI/js/attendant.js
+++ UI/js/attendant.js
@@ -13,6 +13,7 @@ window.onload = function get_products(){
     })
     .then(response => response.json())
     .then((result) => {
+            console.log(result)
             let products = result.products;
             let products_table = document.getElementById('products_table');
             products.forEach(product => { 
@@ -21,44 +22,53 @@ window.onload = function get_products(){
                 price = product.price;
                 quantity = product.product_quantity;
 
-                products_table.innerHTML += '<div class="single_product_category">'+
-                        '<div class="product_image"><img src="../images/laptop1.jpeg" alt="laptops" width="210" height="137"></div>'+
-                        '<div class="product_category_descr">'+product_name+'<br>'+
-                        '<button style="float:left;background-color: white;padding: 4px;">Ksh. '+price+'</button>'+
-                        '<button style="float:right;background-color: white;padding: 4px;"><b>'+quantity+'</b> Available</button>'+
-                            '<br><br>'+
-                            '<a href="single_product.html" class="add_to_cart_btn">'+
-                                '<button onclick="show_product('+id+')" id="myBtn">View</button>'+
-                            '</a>'+
-                            '<button class="add_to_cart_buttons" onclick="postsales('+quantity+','+price+')">Post <b>1</b> Sale</button>'+
-                        '</div>'+
-                '</div>'
+                if(quantity >= 1){
+                    products_table.innerHTML += '<div class="single_product_category">'+
+                            '<div class="product_image"><img src="../images/laptop1.jpeg" alt="laptops" width="210" height="137"></div>'+
+                            '<div class="product_category_descr">'+product_name+'<br>'+
+                            '<button style="float:left;background-color: white;padding: 4px;">Ksh. '+price+'</button>'+
+                            '<button style="float:right;background-color: white;padding: 4px;"><b>'+quantity+'</b> Available</button>'+
+                                '<br><br>'+
+                                '<a href="single_product.html" class="add_to_cart_btn">'+
+                                    '<button onclick="show_product('+id+')" id="myBtn">View</button>'+
+                                '</a>'+
+                                '<button class="add_to_cart_buttons" onclick="postsales('+id+')">Post <b>1</b> Sale</button>'+
+                            '</div>'+
+                    '</div>'
+                }
+                // else{
+                //     document.getElementById('error-display').innerHTML = '<div style="padding:80px;"><center><h1>No Products available</h1></center></div>';
+                //     document.getElementById('success-display').style.display = 'none'
+                // }
             })
     })
 } 
 // onclick="post_sale('+price+','+quantity+')
 
-function postsales(quantity, price){
-    fetch('https://nick-storemanager.herokuapp.com/sales',{
+function postsales(id){
+    url = 'https://nick-storemanager.herokuapp.com/sale/'.concat(id);
+    // console.log(url)
+    fetch(url,{
         method:'POST',
         headers:{
             'Accept':'application/json',
             'Content-type':'application/json',
             'Access-Control-Allow-Origin': '*',
             'Authorization':'Bearer ' + token
-        },
-        body:JSON.stringify({product_name:product_name, product_quantity:quantity, price:price})
+        }
     })
     .then(result =>  result.json().then(data => ({status: result.status, body: data})))
     .then(result => {
+        console.log(result)
         if(result.status == 201){
-            document.getElementById('success-display').innerHTML = result.body.message;
+            alert("Sale successfully posted")
+            window.location.reload();
         }else if(result.status == 404){
-            document.getElementById('error-display').innerHTML = result.body.message;;
-        }else if(result.status == 409){
-            document.getElementById('error-display').innerHTML = result.body.message;;
+            document.getElementById('error-display').innerHTML = result.body.message;
+        }else if(result.status == 413){
+            document.getElementById('error-display').innerHTML = result.body.message;
         }else{
-            document.getElementById('error-display').innerHTML = "something wrong happened";
+            document.getElementById('error-display').innerHTML = "something wrong happened, Try again";
         }
     })
 }
@@ -84,29 +94,6 @@ function display_product(){
     pr = localStorage.getItem('product_name')
     document.getElementById('display_product').innerHTML = "<b>Product Name  </b>:" + pr;
 }
-
-function post_sale(price, quantity) {
-        console.log(price, quantity, product_name);
-    
-        fetch('https://nick-storemanager.herokuapp.com/sales',{
-            method:'POST',
-            headers:{
-                'Accept':'application/json',
-                'Content-type':'application/json',
-                'Access-Control-Allow-Origin': '*',
-                'Authorization':'Bearer ' + token
-            },
-            body:JSON.stringify({product_name:product_name, product_quantity:quantity, price:price})
-        })
-        .then(result =>  result.json().then(data => ({status: result.status, body: data})))
-        .then(result => {
-            console.log(result)
-            product = document.getElementById('display_product');
-            pr = result.body;
-            product.innerHTML=pr;
-        })
-}
-
 
 // get sale records
 // window.onload = function get_sales(){
